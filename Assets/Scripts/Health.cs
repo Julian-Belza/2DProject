7499bc30fb43087e5e29aea904de8a3bf7b42787
MichaelElliottJ <53451624+MichaelElliottJ@users.noreply.github.com>
2023-11-11 02:21:46 -0500

    THE ENEMIES WORK FINALLY

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] public float startingHealth;
     public float currentHealth { get; private set;}
 
     private Animator anim;
-    bool dead = false;
+    private bool dead;
+
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+    [Header ("components")]
+    [SerializeField] private Behaviour[] components;
 
     private void Awake()
     {
         currentHealth = startingHealth;
 
         anim = GetComponent<Animator>();
+
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -24,19 +35,19 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("Hurt");
+            StartCoroutine(Invunerability());
         } else 
         {
             if (!dead)
             {
                 anim.SetTrigger("Death");
-                if (GetComponent<HeroKnight>() != null)
-                    GetComponent<HeroKnight>().enabled = false;
-
-                if (GetComponentInParent<EnemyPatrol>() != null)
-                    GetComponentInParent<EnemyPatrol>().enabled = false;
-
-                if (GetComponent<Enemy>() != null)
-                    GetComponent<Enemy>().enabled = false;
+                foreach (Behaviour component in components)
+                {
+                    component.enabled = false;
+                    GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+                    GetComponent<BoxCollider2D>().enabled = false;
+                }
+                    
                 dead = true;
             }
         }
@@ -45,5 +56,18 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(6,7, true);
+        for (int i = 0; i <numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1,1,1, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(6,7, false);
     }
 }
